bf5fbdcc9696aac8f6f1e610b7d7c0f9e97012ef
BzzzThe18th <69125495+BzzzThe18th@users.noreply.github.com>
2023-12-06 22:53:36 -0600

    push local

--- Main.cs
+++ Main.cs
@@ -7,25 +7,64 @@ namespace CommandLib
 {
     public class ModInfo
     {
-        public const string name = "Unnamed Mod";
-        public const string guid = "author.unnamedmod";
+        public const string name = "Command Library";
+        public const string guid = "buzzbb.cmdlib";
         public const string version = "1.0.0";
     }
 
     [BepInPlugin(ModInfo.guid, ModInfo.name, ModInfo.version)]
-    public class Main : BaseUnityPlugin
+    public class Commands : BaseUnityPlugin
     {
         private Harmony harmonyInstance = new Harmony(ModInfo.guid);
-
         void Awake()
         {
             Logger.LogInfo($"Reached awake point for {ModInfo.name}@{ModInfo.version}, applying patches");
-            /*
-                this applies all harmony patches, to unpatch, use harmonyInstance.UnpatchSelf()
-                this template includes an example patch to learn from
-            */
             harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
             Logger.LogInfo("Applied patches");
+        }
+
+        /*
+            if you want to just make a command, use this, word is the word that triggers it, return text is what it spits back, event name is the terminal event name (exmaple: https://github.com/BzzzThe18th/CustomTerminal/blob/main/Patches/TerminalPatches.cs)
+        */
+        public static void RegisterCommand(string word, string returnText, string eventName, Terminal terminal)
+        {
+            TerminalKeyword keyword = MakeKeyword(word.ToLower());
+            TerminalNode node = MakeNode(returnText, eventName);
+            keyword.specialKeywordResult = node;
+            RegisterKeyword(keyword, terminal);
+        }
+
+        /*
+            both of these are just creating scriptable object instances and setting the variables
+        */
+        public static TerminalKeyword MakeKeyword(string word)
+        {
+            
+            TerminalKeyword keyword = ScriptableObject.CreateInstance<TerminalKeyword>();
+            keyword.word = word;
+            return keyword;
+        }
+        public static TerminalNode MakeNode(string returnText, string associatedEvent) {
+            TerminalNode node = ScriptableObject.CreateInstance<TerminalNode>();
+            node.displayText = returnText;
+            node.clearPreviousText = true;
+            node.terminalEvent = associatedEvent;
+            return node;
+        }
+        
+        /*
+            just add the keyword into the terminal list
+        */
+        public static void RegisterKeyword(TerminalKeyword keyword, Terminal terminal)
+        {
+            if (terminal != null)
+            {
+                terminal.terminalNodes.allKeywords = terminal.terminalNodes.allKeywords.AddToArray(keyword);
+            }
+            else
+            {
+                Debug.LogError("Tried to register keyword with null terminal");
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# LethalCompanyModTemplate
- Mod template for lethal company
+
+# Command Library
+
+Simple, probably bad library to register terminal commands.
+
+## How do I register a command?
+
+Use `Commands.RegisterCommand` and pass in the word that you want to trigger the command, the text to return when it is executed, the name of the terminal event (if you don't want to use this, just input an empty string), and your terminal instance.
+
+## Why the fuck did you make such a small library?
+
+I don't like having something that could be separated and reused incorporated in a mod.
